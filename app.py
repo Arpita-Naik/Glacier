e5330b6aa06703a7cf0c1ab6a038c97209620129
prajwalshettar43 <prajwalshettar43@gmail.com>
2025-01-20 18:13:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
+import os
 from flask import Flask, send_from_directory
-from flask_cors import CORS  # Import Flask-CORS
-from routes.snow_route import snow_route  # Import snow_route blueprint
-from routes.pdd_route import pdd_route  # Import pdd_route blueprint
+from flask_cors import CORS
+from routes.snow_route import snow_route
+from routes.pdd_route import pdd_route
 from routes.seismic_route import seismic_route
 from routes.register import reg
-app = Flask(__name__)
 
-# Enable CORS for all routes
+app = Flask(__name__)
 CORS(app)
 
-# Register the blueprints for snow and PDD predictions
-app.register_blueprint(snow_route, url_prefix='/snow')  # /snow route for snow predictions
-app.register_blueprint(pdd_route, url_prefix='/pdd')    # /pdd route for PDD predictions
+app.register_blueprint(snow_route, url_prefix='/snow')
+app.register_blueprint(pdd_route, url_prefix='/pdd')
 app.register_blueprint(seismic_route, url_prefix='/seismic')
 app.register_blueprint(reg, url_prefix='/reg')
-# Serve static HTML file
+
 @app.route('/')
 def index():
     return send_from_directory('templates', 'index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000 for local testing
+    app.run(host="0.0.0.0", port=port, debug=True)
