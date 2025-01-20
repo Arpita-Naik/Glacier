c134f3a014e556ddd49e01dd613bf28c88bf496b
prajwalshettar43 <prajwalshettar43@gmail.com>
2025-01-20 18:04:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.register_blueprint(reg, url_prefix='/reg')
 # Serve static HTML file
 @app.route('/')
 def index():
-    return send_from_directory('static', 'snow_predict.html')
+    return send_from_directory('templates', 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
